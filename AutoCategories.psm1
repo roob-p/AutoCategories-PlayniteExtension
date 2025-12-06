a41b7e6caec3b202dc41fdfd9bb09b68ec2acc35
roob-p <roberto_p88@yahoo.it>
2025-12-06 17:54:20 +0100

    Add files via upload

--- AutoCategories.psm1
+++ AutoCategories.psm1
@@ -60,7 +60,9 @@ $nomibrevi = @{}
 
 	Get-Content -Path "$PSScriptRoot\Platforms.txt" | ForEach-Object {
 	$parts = $_ -split "="
-    $nomibrevi[$parts[0].Trim()] = $parts[1].Trim()
+    $platform = $parts[0].Trim()
+    $names = $parts[1].Trim() -split ";"
+    $nomibrevi[$platform] = $names
 }
 
 
@@ -72,107 +74,53 @@ if($catName -ne $null) {$catNameS=$nomibrevi[$catname]}
 
 
 
-if (!$game.source.name){ #Se la sorgente è vuota
+if (!$game.source.name) {  # Se la sorgente è vuota
+    if ($catName -ne $null) {  # Se la piattaforma del gioco non è vuota
+        if ($nomibrevi.ContainsKey($catName)) { #Se la categoria esiste già aggiunge il gioco alla categoria, altrimenti crea una nuova categoria col nome della piattaforma.
+            $nomibrevi[$catName] | ForEach-Object {
+                $category = $PlayniteApi.Database.Categories.Add($_)
+                if ($gameI.CategoryIds -eq $null -or !$gameI.CategoryIds.Contains($category.Id)) {
+                    $gameI.CategoryIds += $category.Id
+                }
+                $PlayniteApi.Database.Games.Update($gameI)
+                $arrgame += $game.name
+                $arrcat += $category.name
+            }
+        } else {
+            $category = $PlayniteApi.Database.Categories.Add($catName)
+            if ($gameI.CategoryIds -eq $null -or !$gameI.CategoryIds.Contains($category.Id)) {
+                $gameI.CategoryIds += $category.Id
+            }
+            $PlayniteApi.Database.Games.Update($gameI)
+            $arrgame += $game.name
+            $arrcat += $category.name
+        }
+    } else {
+        $arrgame += $game.name
+        $arrcat += "SENZA PIATTAFORMA"
+    }
+} else {  # Se c'è una sorgente
+    $sou = $game.source.name
+    $sorgente = @{}
+    Get-Content -Path "$PSScriptRoot\Source.txt" | ForEach-Object {
+        $parts = $_ -split "="
+        $sorgente[$parts[0].Trim()] = $parts[1].Trim()
+    }
 
-if ($catName -ne $null){ #se la piattaforma del gioco non è vuota
+    if ($sorgente.ContainsKey($sou)) {
+        $category = $PlayniteApi.Database.Categories.Add($sorgente[$sou])
+    } else {
+        $category = $PlayniteApi.Database.Categories.Add($sou)
+    }
 
-if($nomibrevi.Containskey($catName)){ #Se la categoria esiste già aggiunge il gioco alla categoria, altrimenti crea una nuova categoria col nome della piattaforma.
-	
-
-
-$category = $PlayniteApi.Database.Categories.Add($catNameS)
-
-
-
-}else{
-
-$category = $PlayniteApi.Database.Categories.Add($catName)
-
+    if ($gameI.CategoryIds -eq $null -or !$gameI.CategoryIds.Contains($category.Id)) {
+        $gameI.CategoryIds += $category.Id
+    }
+    $PlayniteApi.Database.Games.Update($gameI)
+    $arrgame += $game.name
+    $arrcat += $category.name
 }
-
-
-
-if ($gameI.CategoryIds -ne $null) {
-if ($gameI.CategoryIds.Contains($category.Id)) {
-   
-} else {
-   
- 
-   $gameI.CategoryIds += $category.Id
-
-
-
-}
-
-} else{ #Categoria nulla:
-	 
-	
-	$gameI.CategoryIds=$null # "Svuota" cosa in realtà non era stato eliminato.
-	$gameI.CategoryIds += $category.Id
-	
-	
-#$arrgame+=$game.name    #!!!
-#$arrcat+=$category.name
-	
-
-
-}
-
-
-$PlayniteApi.Database.Games.Update($gameI)
-$arrgame+=$game.name    #!!!
-$arrcat+=$category.name
-
-}#endif la piattaforma esiste
-else { #la piattaforma non esiste
-	
-$arrgame+=$game.name
-$arrcat+="SENZA PIATTAFORMA!"
-	
-}
-
-}#endif source empty check
-else{ #Se c'è la sorgente:
-
-	$sou=$game.source.name  
-	
-	$sorgente = @{}
-
-	Get-Content -Path "$PSScriptRoot\Source.txt" | ForEach-Object {
-	$parts = $_ -split "="
-    $sorgente[$parts[0].Trim()] = $parts[1].Trim()
-}
-	
-	
-	$souS=$sorgente[$sou]
-	
-	if($sorgente.Containskey($sou)){ #Se la categoria esiste già aggiunge il gioco alla categoria, altrimenti crea una nuova categoria col nome della sorgente.
-		$category = $PlayniteApi.Database.Categories.Add($souS)
-	}else{
-		$category = $PlayniteApi.Database.Categories.Add($sou)
-		#$category = $PlayniteApi.Database.Categories.Add($game.source)
-}
-if ($gameI.CategoryIds -ne $null) {
-if ($gameI.CategoryIds.Contains($category.Id)) {
-    
-} else {
-   
-   $gameI.CategoryIds += $category.Id
-}
-
-} else{ #Categoria nulla:
-
-	$gameI.CategoryIds=$null # "Svuota" cosa in realtà non era stato eliminato.
-	$gameI.CategoryIds += $category.Id
-}
-		$PlayniteApi.Database.Games.Update($gameI)
-		
-		
-$arrgame+=$game.name    #!!!
-$arrcat+=$category.name
-		
-
-}#else (sorgente)
+#else (sorgente)
 	
 
 	
@@ -190,7 +138,7 @@ $arrcat+=$category.name
 
 
 
-
+<#
 foreach ($categoria in $arrcat) {
 	#$PlayniteApi.Dialogs.ShowMessage("3")
   
@@ -223,4 +171,33 @@ foreach ($categoria in $arr.Keys) {
 
 
 $PlayniteApi.Dialogs.ShowMessage($output)
+
+#>
+
+# Creazione di un array associato per evitare duplicazioni
+foreach ($categoria in $arrcat) {
+    $giochi = @()
+    for ($i = 0; $i -lt $arrgame.Count; $i++) {
+        if ($arrcat[$i] -eq $categoria) {
+            $giochi += $arrgame[$i]
+        }
+    }
+    $arr[$categoria] = $giochi
+}
+
+# Creazione del messaggio di output
+$output = ""
+foreach ($categoria in $arr.Keys) {
+    $output += "$categoria" + ": "
+    $output += '"' + $($arr[$categoria] -join '", "') + '"' + "`n`n"
+}
+
+# Visualizza il risultato finale
+$PlayniteApi.Dialogs.ShowMessage($output)
+
+
+
+
+
+
 } #end func
